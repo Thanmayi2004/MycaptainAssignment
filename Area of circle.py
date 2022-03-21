e03f5788d5feea5ae56b16da579f5c4068990457
Thanmayi2004 <101060022+Thanmayi2004@users.noreply.github.com>
2022-03-21 18:21:56 +0530

    Add files via upload

--- Area of circle.py
+++ Area of circle.py
@@ -1,5 +1,5 @@
 Radius=float(input("Enter the radius : "));
 area=3.14*Radius*Radius;
-print("The area of circle with radius" " " + str(Radius) + " " "is:" " " +str(area));
+print("The area of circle with radius" " " , str(Radius) , " " "is:" " " ,area);
 
 
